3f908a6dc1ddea2cfca0141e98e554c1e7b38e21
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-02-05 13:44:24 +0200

    Madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,16 @@
 def main():
 	# write your code here
 
+	time = input("Enter a Time:  ")
+	items = input("Enter an Item:  ")
+	name = input("Enter a Name:  ")
+	scream = input("Enter any Sentence:  ")
+	action = input("Enter an Action:  ")
+
+	print("It was " + time + " o'clock when I heard a knock at the door.")
+	print('I opened the door and there was a box of ' + items + ' with a note saying \"From Mr. ' + name.title() + '."')
+	print('Just as I closed the door I heard a scream" ' + scream.upper()+ '."')
+	print("I froze in place and all I could do was " + action + ".")
 
 
 if __name__ == '__main__':
